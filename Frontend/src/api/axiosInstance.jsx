1be6af1f61e35217da016499cdd31a89c72e18a6
Abhishek Bhatia <145684624+abhishekbhatia01@users.noreply.github.com>
2026-05-01 22:31:28 +0530

    Update axiosInstance.jsx

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://bookstore1-nxyc.onrender.com/api",
+  baseURL: "http://localhost:5000/api/api",
 });
 
 axiosInstance.interceptors.request.use(
